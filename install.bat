91a025d6cb823b000c1c85a7cb758846283d2954
endman100 <endman246738@gmail.com>
2024-08-16 10:37:38 +0800

    update

--- install.bat
+++ install.bat
@@ -5,9 +5,13 @@ git clone https://github.com/kohya-ss/sd-scripts.git
 cd sd-scripts
 git pull
 pip install -U -r requirements.txt
+git checkout sd3
 cd ..
 
-pip install https://github.com/jllllll/bitsandbytes-windows-webui/releases/download/wheels/bitsandbytes-0.40.1.post1-py3-none-win_amd64.whl
-pip install https://github.com/bdashore3/flash-attention/releases/download/v2.6.3/flash_attn-2.6.3+cu123torch2.4.0cxx11abiFALSE-cp310-cp310-win_amd64.whl
 pip install -U xformers --index-url https://download.pytorch.org/whl/cu121
 pip install -U torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+pip install numpy==2.0.0
+pip install sentencepiece
+pip install wandb
+pip install https://github.com/jllllll/bitsandbytes-windows-webui/releases/download/wheels/bitsandbytes-0.40.1.post1-py3-none-win_amd64.whl
+pip install https://github.com/bdashore3/flash-attention/releases/download/v2.6.3/flash_attn-2.6.3+cu123torch2.4.0cxx11abiFALSE-cp310-cp310-win_amd64.whl
