1af2c4c1687792b577146c8489a7b74c8e04715a
endman100 <endman246738@gmail.com>
2024-11-12 10:41:15 +0800

    兼容外部呼叫

--- install.bat
+++ install.bat
@@ -1,20 +1,7 @@
-python -m venv venv
-call .\venv\Scripts\activate
-
 git clone https://github.com/kohya-ss/sd-scripts.git
 cd sd-scripts
 git pull
-pip install -U -r requirements.txt
 git checkout sd3
 cd ..
 
-pip install -U xformers --index-url https://download.pytorch.org/whl/cu121
-pip install -U torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
-pip install "numpy<2"
-pip install -U opencv-python
-pip install sentencepiece
-pip install wandb
-pip install https://github.com/jllllll/bitsandbytes-windows-webui/releases/download/wheels/bitsandbytes-0.40.1.post1-py3-none-win_amd64.whl
-pip install https://github.com/bdashore3/flash-attention/releases/download/v2.6.3/flash_attn-2.6.3+cu123torch2.4.0cxx11abiFALSE-cp310-cp310-win_amd64.whl
 
-@REM wandb login
--- train_with_resume.py
+++ train_with_resume.py
@@ -5,25 +5,28 @@ import json
 import time
 import os
 import subprocess
-output_name = "Anime Pussy"
-max_train_epochs = 10
-pretrained_model_name_or_path = r"C:\ComfyUIModel\models\checkpoints\flux1-dev.safetensors"
-clip_l = r"C:\ComfyUIModel\models\clip\clip_l.safetensors"
-t5xxl = r"C:\ComfyUIModel\models\clip\t5xxl_fp16.safetensors"
-ae = r"C:\ComfyUIModel\models\vae\ae.safetensors"
-# dataset_config = r"../config_animep.toml"
-dataset_config = r"../config_lin.toml"
-# output_dir = r"D:\AIGC\dataset\fun\Anime Pussy\model_faster_small"
-output_dir = r"D:\AIGC\dataset\fun\LilyLinglan\LilyLinglan_model_faster_v2"
-
-learning_rate = 5e-5
-network_dim = 8
 
 
-def get_command(initial_epoch, resume):
+venv_activate_path = os.path.abspath('./venv/Scripts/activate')
+def get_command(initial_epoch, resume, **kwargs):
+    pretrained_model_name_or_path = kwargs.get("pretrained_model_name_or_path", r"C:\ComfyUIModel\models\checkpoints\flux1-dev.safetensors")
+    clip_l = kwargs.get("clip_l", r"C:\ComfyUIModel\models\clip\clip_l.safetensors")
+    t5xxl = kwargs.get("t5xxl", r"C:\ComfyUIModel\models\clip\t5xxl_fp16.safetensors")
+    ae = kwargs.get("ae", r"C:\ComfyUIModel\models\vae\ae.safetensors")
+    max_train_epochs = kwargs.get("max_train_epochs", 10)
+    learning_rate = kwargs.get("learning_rate", 5e-5)
+    network_dim = kwargs.get("network_dim", 8)
+    dataset_config = kwargs["dataset_config"]
+    output_dir = kwargs["output_dir"]
+    output_name = kwargs["output_name"]
+
+    py_dir_path = os.path.dirname(os.path.abspath(__file__))
+    train_py_path = os.path.join(py_dir_path,"sd-scripts", "flux_train_network.py")
+
+
     if resume != "":
         keep_cmd = f'\
-        accelerate launch  --mixed_precision bf16 --num_cpu_threads_per_process 1 flux_train_network.py \
+        {venv_activate_path} && accelerate launch --mixed_precision bf16 --num_cpu_threads_per_process 1 {train_py_path} \
             --pretrained_model_name_or_path="{pretrained_model_name_or_path}" \
             --clip_l="{clip_l}" \
             --t5xxl="{t5xxl}" \
@@ -42,7 +45,7 @@ def get_command(initial_epoch, resume):
             --lowram --save_state '
     else:
         keep_cmd = f'\
-        accelerate launch  --mixed_precision bf16 --num_cpu_threads_per_process 1 flux_train_network.py \
+        {venv_activate_path} && accelerate launch  --mixed_precision bf16 --num_cpu_threads_per_process 1 {train_py_path} \
             --pretrained_model_name_or_path="{pretrained_model_name_or_path}" \
             --clip_l="{clip_l}" \
             --t5xxl="{t5xxl}" \
@@ -59,38 +62,43 @@ def get_command(initial_epoch, resume):
             --lowram --save_state '
     return keep_cmd
 
-log_path = os.path.join(output_dir, "train.log")
-while(True):
-    print("run_command")
-    dir_path = os.path.dirname(log_path)
-    if not os.path.exists(dir_path) and dir_path != "":
-        os.makedirs(dir_path)
-    
-    #檢查是否有儲存點
-    max_epoch = -1
-    max_resume = ""
-    if(os.path.exists(output_dir)):
-        for output_file in os.listdir(output_dir):
-            output_filepath = os.path.join(output_dir, output_file)
-            print(output_filepath)
-            if(os.path.isdir(output_filepath)):
-                name, epoch, state = output_file.split("-")
-                epoch = int(epoch)
-                if (max_epoch < epoch):
-                    max_epoch = epoch
-                    max_resume = output_filepath
-    print(f"max_epoch: {max_epoch}, max_resume: {max_resume}")
+def train_with_resume(output_name, dataset_config, output_dir, **kwargs):
+    log_path = os.path.join(output_dir, "train.log")
+    while(True):
+        print("run_command")
+        dir_path = os.path.dirname(log_path)
+        if not os.path.exists(dir_path) and dir_path != "":
+            os.makedirs(dir_path)
+        
+        #檢查是否有儲存點
+        max_epoch = -1
+        max_resume = ""
+        if(os.path.exists(output_dir)):
+            for output_file in os.listdir(output_dir):
+                output_filepath = os.path.join(output_dir, output_file)
+                print(output_filepath)
+                if(os.path.isdir(output_filepath)):
+                    name, epoch, state = output_file.split("-")
+                    epoch = int(epoch)
+                    if (max_epoch < epoch):
+                        max_epoch = epoch
+                        max_resume = output_filepath
+        print(f"max_epoch: {max_epoch}, max_resume: {max_resume}")
+        kwargs["output_name"] = output_name
+        kwargs["dataset_config"] = dataset_config
+        kwargs["output_dir"] = output_dir
+        cmd = get_command(max_epoch, max_resume, **kwargs)
+        with open(log_path, "a", encoding="utf-8") as f:
+            print(f"run_command: {cmd}")
+            # process = subprocess.Popen(cmd, shell=True, stdout=f, stderr=f, text=True)
+            process = subprocess.Popen(cmd, shell=True, text=True)
+            process.wait()
+        
+        if process.returncode != 0:
+            print(f"run_command error: {cmd}")
+            print(f"run again")
+            time.sleep(60)
+        else:
+            break
 
-    cmd = get_command(max_epoch, max_resume)
-    with open(log_path, "a", encoding="utf-8") as f:
-        print(f"run_command: {cmd}")
-        # process = subprocess.Popen(cmd, shell=True, stdout=f, stderr=f, text=True)
-        process = subprocess.Popen(cmd, shell=True, text=True)
-        process.wait()
-    
-    if process.returncode != 0:
-        print(f"run_command error: {cmd}")
-        print(f"run again")
-        time.sleep(60)
-    else:
-        break
+# if __name__ == "__main__":
