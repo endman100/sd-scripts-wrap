90bc6ba1e44c7d073817a795319076a5f2d9dacc
endman100 <endman246738@gmail.com>
2025-07-29 17:57:05 +0800

    Update train_with_resume.py

--- train_with_resume.py
+++ train_with_resume.py
@@ -83,6 +83,7 @@ def get_command_sdxl(initial_epoch, resume, **kwargs):
     dataset_config = kwargs["dataset_config"]
     output_dir = kwargs["output_dir"]
     output_name = kwargs["output_name"]
+    network_module = kwargs.get("network_module", "networks.lora")
     save_every_n_epochs = kwargs.get("save_every_n_epochs", 5)
     gradient_accumulation_steps = 1
 
@@ -95,9 +96,9 @@ def get_command_sdxl(initial_epoch, resume, **kwargs):
             --pretrained_model_name_or_path="{pretrained_model_name_or_path}" \
             --save_model_as safetensors --sdpa --persistent_data_loader_workers \
             --max_data_loader_n_workers 1 --gradient_checkpointing --mixed_precision bf16 --save_precision bf16 \
-            --network_module networks.lora_fa --network_dim={network_dim} --optimizer_type AdamW --learning_rate={learning_rate} \
+            --network_module={network_module} --network_dim={network_dim} --optimizer_type AdamW --learning_rate={learning_rate} \
             --cache_latents_to_disk --cache_text_encoder_outputs_to_disk \
-            ---network_train_unet_only \
+            --network_train_unet_only \
             --highvram --max_train_epochs {max_train_epochs} --save_every_n_epochs={save_every_n_epochs} --dataset_config="{dataset_config}" \
             --output_dir="{output_dir}" \
             --output_name="{output_name}" \
@@ -111,9 +112,9 @@ def get_command_sdxl(initial_epoch, resume, **kwargs):
             --pretrained_model_name_or_path="{pretrained_model_name_or_path}" \
             --save_model_as safetensors --sdpa --persistent_data_loader_workers \
             --max_data_loader_n_workers 1 --gradient_checkpointing --mixed_precision bf16 --save_precision bf16 \
-            --network_module networks.lora_fa --network_dim={network_dim} --optimizer_type AdamW --learning_rate={learning_rate} \
+            --network_module={network_module} --network_dim={network_dim} --optimizer_type AdamW --learning_rate={learning_rate} \
             --cache_latents_to_disk --cache_text_encoder_outputs_to_disk \
-            --cache_text_encoder_outputs --network_train_unet_only  \
+            --network_train_unet_only  \
             --highvram --max_train_epochs {max_train_epochs} --save_every_n_epochs={save_every_n_epochs} --dataset_config="{dataset_config}" \
             --output_dir="{output_dir}" \
             --output_name="{output_name}" \
