7abbe691779e47fb90bf9f0b8125b5d4208700ff
endman100 <endman246738@gmail.com>
2025-11-11 11:48:52 +0800

    Update train_with_resume.py

--- train_with_resume.py
+++ train_with_resume.py
@@ -170,7 +170,10 @@ def get_command_sdxl_clip(initial_epoch, resume, **kwargs):
     return keep_cmd
 
 def get_command_sdxl_suffle(initial_epoch, resume, **kwargs):
-    pretrained_model_name_or_path =  r"C:\ComfyUIModel\models\checkpoints\waiNSFWIllustrious_v140.safetensors"
+    if os.name == 'nt':
+        pretrained_model_name_or_path =  r"C:\ComfyUIModel\models\checkpoints\waiNSFWIllustrious_v140.safetensors"
+    else:
+        pretrained_model_name_or_path =  "/workspace/storage/stable_diffusion/models/ckpts/waiNSFWIllustrious_v140.safetensors"
     wandb_dir = kwargs.get("wandb_dir", r"./wandb")
     max_train_epochs = kwargs.get("max_train_epochs", 6)
     learning_rate = kwargs.get("learning_rate", 1e-4)
@@ -221,7 +224,10 @@ def get_command_sdxl_suffle(initial_epoch, resume, **kwargs):
     return keep_cmd
 
 def get_command_sdxl_all(initial_epoch, resume, **kwargs):
-    pretrained_model_name_or_path =  r"C:\ComfyUIModel\models\checkpoints\waiNSFWIllustrious_v140.safetensors"
+    if os.name == 'nt':
+        pretrained_model_name_or_path =  r"C:\ComfyUIModel\models\checkpoints\waiNSFWIllustrious_v140.safetensors"
+    else:
+        pretrained_model_name_or_path =  "/workspace/storage/stable_diffusion/models/ckpts/waiNSFWIllustrious_v140.safetensors"
     wandb_dir = kwargs.get("wandb_dir", r"./wandb")
     max_train_epochs = kwargs.get("max_train_epochs", 6)
     learning_rate = kwargs.get("learning_rate", 1e-4)
